dc0a1a68f3c99d7aa80d88287396c30f415db731
Solomon Eniola Adegoke <Souceking@gmail.com>
2023-03-20 13:39:49 +0100

    backend with node

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+
+
+const names = require('./4-name');
+const sayHi = require('./5-utilis');
+const data = require('./6-alternative-flavour')
+require('./7-mind-grenade')
+console.log(data)
+console.log(names)
+
+sayHi('Susan')
+sayHi(names.john)
+sayHi(names.peter)
---
+++ 4-name.js
@@ -0,0 +1,8 @@
+//local
+const secret = 'SUPER SECRET'
+// Share
+const john = 'Bro John'
+const peter = 'Bro Peter'
+
+
+module.exports = {john, peter}
