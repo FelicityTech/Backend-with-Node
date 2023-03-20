4c47b1050db66bbdbd7d176c4590ebdd5825b0d2
Solomon Eniola Adegoke <Souceking@gmail.com>
2023-03-20 13:38:53 +0100

    Basic of node

---
+++ introduction-Node/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('content-Type', 'test/plain');
+    res.end('Hello Solomon Eniola!');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
