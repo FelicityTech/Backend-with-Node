40289a952587326e082d5a67d901740985bc1f3a
Solomon Eniola Adegoke <Souceking@gmail.com>
2023-03-21 08:50:43 +0100

    built-in module file

---
+++ introduction-Node/built-module/socket-io.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const socketIo = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
